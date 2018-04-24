8a5c8fb3d9ab2c6615ae0ef045afd9891bbefaa6
zhaohan2 <zhaohan2@illinois.edu>
2018-04-24 15:01:16 +0800

    second draft

--- MainActivity.java
+++ MainActivity.java
@@ -2,40 +2,62 @@ package edu.illinois.cs.cs125.mp7_zhaohan2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    int originalAmount, targetAmount, ratio;
-    String originalCurrency, targetCurrency;
+    private static final String TAG = "MainActivity";
 
-    EditText originalAmountInput;
-    Spinner originalCurrencyInput;
-    Spinner targetCurrencyInput;
-    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Currency, android.R.layout.simple_spinner_dropdown_item);
-
-
+    Spinner originalCurrencyInput, targetCurrencyInput;
+    String originalCurrency;
+    String targetCurrency;
     Button convertButton;
+    EditText originalNumber;
+    double originalAmount;
+    double targetAmount;
+    double ratio;
+    TextView tgt;
+    TextView rate;
+
+
+    ArrayAdapter<String> adapter;
+
+    String currencys[] = {"USD", "CHY", "EUR", "JPY", "AUD"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        originalAmountInput = findViewById(R.id.OriginalNumber);
+        Log.i(TAG, "App Started");
+
+        tgt = findViewById(R.id.TargetNumber);
+        rate = findViewById(R.id.Rate);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, currencys);
+
         originalCurrencyInput = findViewById(R.id.OriginalCurrency);
         targetCurrencyInput = findViewById(R.id.TargetCurrency);
+
         originalCurrencyInput.setAdapter(adapter);
         targetCurrencyInput.setAdapter(adapter);
         originalCurrencyInput.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 originalCurrency = parent.getItemAtPosition(position).toString();
+                if (originalCurrency == targetCurrency) {
+                    ratio = 1;
+                }
+                if (originalCurrency != null && targetCurrency != null) {
+                    rate.setText(String.valueOf(ratio));
+                }
             }
 
             @Override
@@ -47,6 +69,12 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 targetCurrency = parent.getItemAtPosition(position).toString();
+                if (originalCurrency == targetCurrency) {
+                    ratio = 1;
+                }
+                if (originalCurrency != null && targetCurrency != null) {
+                    rate.setText(String.valueOf(ratio));
+                }
             }
 
             @Override
@@ -55,12 +83,21 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        originalNumber = findViewById(R.id.OriginalNumber);
+
+
         convertButton = findViewById(R.id.Convert);
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                originalAmount = Integer.valueOf(originalAmountInput.getText().toString());
+                Log.d(TAG, "Button clicked");
+                if (findViewById(R.id.OriginalNumber).toString() != null) {
+                    originalAmount = Integer.valueOf(originalNumber.getText().toString());
+                    targetAmount = originalAmount * ratio;
+                    tgt.setText(String.valueOf(targetAmount));
+                }
             }
         });
+
     }
 }
