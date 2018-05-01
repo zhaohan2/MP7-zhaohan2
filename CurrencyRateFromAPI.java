e6bef8837b533a42419127ca5ff1afe1c26510b1
zhaohan2 <zhaohan2@illinois.edu>
2018-05-01 14:18:30 +0800

    done

--- CurrencyRateFromAPI.java
+++ CurrencyRateFromAPI.java
@@ -1,10 +1,26 @@
 package edu.illinois.cs.cs125.mp7_zhaohan2;
 
+import java.net.URI;
+
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+
+
 public class CurrencyRateFromAPI {
+
 
 
     public CurrencyRateFromAPI() {
     }
+
+    String webpage = "https://free.currencyconverterapi.com/api/v5/convert";
+
+
 
     public float getRate(final String currency1, final String currency2) {
         float rate = 1;
@@ -13,8 +29,37 @@ public class CurrencyRateFromAPI {
         } else if (currency1 == currency2) {
             return rate;
         } else {
-            //rate = get ratio from API
+            /*
+            try {
+
+                URIBuilder builder = new URIBuilder(webpage);
+
+                String parameter1 = currency1 + "_" + currency2;
+                builder.setParameter("q", currency1 + "_" + currency2 );
+                builder.setParameter("compact", "y");
+
+                URI uri = builder.build();
+                System.out.println(uri);
+                HttpPost request = new HttpPost(uri);
+
+                HttpResponse response = httpclient.execute(request);
+                HttpEntity entity = response.getEntity();
+
+                if (entity != null) {
+                    // Format and display the JSON response.
+                    String jsonString = EntityUtils.toString(entity);
+                    JsonParser parser = new JsonParser();
+                    JsonObject rootObj = parser.parse(jsonString).getAsJsonObject();
+                    JsonObject locObj = rootObj.getAsJsonObject(parameter1);
+                    rate = locObj.get("val").getAsFloat();
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            //rate = get ratio from API*/
         }
+
         return rate;
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -11,6 +11,19 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -25,11 +38,22 @@ public class MainActivity extends AppCompatActivity {
     float ratio;
     TextView tgt;
     TextView rate;
+    String parameter1 = "USD_USD";
+    String webpage =
+            "https://free.currencyconverterapi.com/api/v5/convert?q=USD_USD&compact=y";
 
+    public static RequestQueue requestQueue;
+
+
+    public static JsonObjectRequest objectRequest;
+
+
+    public static String webpage0;
 
     ArrayAdapter<String> adapter;
 
-    String currencys[] = {"USD", "CHY", "EUR", "JPY", "AUD"};
+    String currencys[] = {"USD", "CNY", "EUR", "JPY", "AUD", "TWD", "HKD", "CAD"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +61,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(TAG, "App Started");
+
+        webpage0 = "https://free.currencyconverterapi.com/api/v5/convert?q=";
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+
+
 
         tgt = findViewById(R.id.TargetNumber);
         rate = findViewById(R.id.Rate);
@@ -56,7 +88,41 @@ public class MainActivity extends AppCompatActivity {
                     ratio = 1;
                 }
                 if (originalCurrency != null && targetCurrency != null) {
-                    rate.setText(String.valueOf(ratio));
+                    parameter1 = originalCurrency + "_" + targetCurrency;
+                    webpage = webpage0 + parameter1 + "&compact=y";
+                    objectRequest = new JsonObjectRequest(
+                            Request.Method.GET,
+                            webpage,
+                            null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.e("REST response", response.toString());
+                                    Log.e("parameter1", parameter1);
+                                    Log.e("originalAmount", String.valueOf(originalAmount));
+                                    Log.e("finalAmount", String.valueOf(targetAmount));
+                                    //try {
+                                    // Format and display the JSON response.
+                                    String jsonString = response.toString();
+                                    JsonParser parser = new JsonParser();
+                                    JsonObject rootObj = parser.parse(jsonString).getAsJsonObject();
+                                    JsonObject root2Obj = rootObj.get(parameter1).getAsJsonObject();
+                                    ratio = root2Obj.get("val").getAsFloat();
+                                    rate.setText(String.valueOf(ratio));
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("REST response", error.toString());
+                                }
+                            });
+
+
+                    RequestQueue mQueue = requestQueue;
+                    mQueue.add(objectRequest);
+                    Log.e("URL", webpage);
+                    Log.e("Rate", String.valueOf(ratio));
                 }
             }
 
@@ -65,6 +131,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
         targetCurrencyInput.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,9 +141,44 @@ public class MainActivity extends AppCompatActivity {
                     ratio = 1;
                 }
                 if (originalCurrency != null && targetCurrency != null) {
-                    rate.setText(String.valueOf(ratio));
+                    parameter1 = originalCurrency + "_" + targetCurrency;
+                    webpage = webpage0 + parameter1 + "&compact=y";
+                    objectRequest = new JsonObjectRequest(
+                            Request.Method.GET,
+                            webpage,
+                            null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.e("REST response", response.toString());
+                                    Log.e("parameter1", parameter1);
+                                    Log.e("originalAmount", String.valueOf(originalAmount));
+                                    Log.e("finalAmount", String.valueOf(targetAmount));
+                                    //try {
+                                    // Format and display the JSON response.
+                                    String jsonString = response.toString();
+                                    JsonParser parser = new JsonParser();
+                                    JsonObject rootObj = parser.parse(jsonString).getAsJsonObject();
+                                    JsonObject root2Obj = rootObj.get(parameter1).getAsJsonObject();
+                                    ratio = root2Obj.get("val").getAsFloat();
+                                    rate.setText(String.valueOf(ratio));
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("REST response", error.toString());
+                                }
+                            });
+
+
+                    RequestQueue mQueue = requestQueue;
+                    mQueue.add(objectRequest);
+                    Log.e("URL", webpage);
+                    Log.e("Rate", String.valueOf(ratio));
                 }
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -86,17 +189,59 @@ public class MainActivity extends AppCompatActivity {
         originalNumber = findViewById(R.id.OriginalNumber);
 
 
+
         convertButton = findViewById(R.id.Convert);
         convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Button clicked");
-                if (findViewById(R.id.OriginalNumber).toString() != null) {
-                    originalAmount = Integer.valueOf(originalNumber.getText().toString());
-                    CurrencyRateFromAPI calculator = new CurrencyRateFromAPI();
-                    ratio = calculator.getRate(originalCurrency,targetCurrency);
-                    targetAmount = originalAmount * ratio;
-                    tgt.setText(String.valueOf(targetAmount));
+                try {
+                    Log.d(TAG, "Button clicked");
+                    if (findViewById(R.id.OriginalNumber).toString() != null) {
+
+                        originalAmount = Float.parseFloat(originalNumber.getText().toString());
+
+                        /*objectRequest = new JsonObjectRequest(
+                                Request.Method.GET,
+                                webpage,
+                                null,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        Log.e("REST response", response.toString());
+                                        Log.e("parameter1", parameter1);
+                                        Log.e("originalAmount", String.valueOf(originalAmount));
+                                        Log.e("finalAmount", String.valueOf(targetAmount));
+                                        //try {
+                                        // Format and display the JSON response.
+                                        String jsonString = response.toString();
+                                        JsonParser parser = new JsonParser();
+                                        JsonObject rootObj = parser.parse(jsonString).getAsJsonObject();
+                                        JsonObject root2Obj = rootObj.get(parameter1).getAsJsonObject();
+                                        ratio = root2Obj.get("val").getAsFloat();
+                                        rate.setText(String.valueOf(ratio));*/
+                                        originalAmount = Integer.valueOf(originalNumber.getText().toString());
+                                        targetAmount = originalAmount * ratio;
+                                        tgt.setText(String.valueOf(targetAmount));
+                                        /*Log.e("json", jsonString);
+                                        Log.e("ratio",String.valueOf(ratio));
+                        /*} catch (NullPointerException e) {
+                            Log.d("API", "Nurupo");
+                        }*/
+                                  /*  }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Log.e("REST response", error.toString());
+                                    }
+                                });
+
+
+                        RequestQueue mQueue = requestQueue;
+                        mQueue.add(objectRequest);*/
+                    }
+                } catch (NullPointerException e) {
+                    Log.d("click", "Nurupo");
                 }
             }
         });
